f1de83e21064a5c6e81b3d1f132bebc0e0b4913b
Nonvignon <83883873+Cyrilleoc@users.noreply.github.com>
2023-01-17 18:46:43 -0500

    ECS

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
-output "ecr_image_url" {
-  value = aws_ecr_repository.ecr_lab
+# outputs.tf
+
+# output "ecr_image_url" {
+#   value = aws_ecr_repository.ecr_lab.repository_url
+# }
+
+output "alb_hostname" {
+  value = aws_alb.main.dns_name
 }
+
